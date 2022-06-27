b5069a52f3dc0faeaf46c3e9278704fc994dc35a
VictorHugoCid <victorcidpereira@gmail.com>
2022-06-27 09:46:39 -0300

    Final 27-06-2022 as 9h45mim

--- scripts.js
+++ scripts.js
@@ -1,28 +1,24 @@
 
-//let namePrompt = prompt("diga seu lindo nome")
+let namePrompt;
 
-let namePrompt = {
-    name: "otoNome"
-}
+/* namePrompt = {
+    name: "llele"
+} */
 
 let msg;
 let nomeDestino;
 let msgServidor = [];
 let participantes = [];
 
-//logar();
-//buscarParticipantes();
-//setInterval(manterLog, 5000);
-//setInterval(buscarMensagens, 3000)
+function obterNome(){
+    let nomezin = document.querySelector(".entrada input").value
+    namePrompt = {
+        name: nomezin
+    }
+    logar();
+}
 
 function logar() {
-    //const nomezin = document.querySelector(".entrada input").value
-    const nomezin = "Rod"
-    console.log(nomezin)
-
-    /* let namePrompt = {
-        name: nomezin
-    } */
     /* primeiro post pro API */
     const promise = axios.post(`https://mock-api.driven.com.br/api/v6/uol/participants`, namePrompt)
 
@@ -30,11 +26,10 @@ function logar() {
         .catch(erroLogin)
         .then(buscarMensagens)
 
-    /* frufruDoLogin();
+    frufruDoLogin();
     setInterval(manterLog, 5000);
-    setInterval(buscarParticipantes,10000);
-    setInterval(buscarMensagens, 3000); */
-
+    setInterval(buscarParticipantes, 10000);
+    setInterval(buscarMensagens, 3000);
 }
 
 function frufruDoLogin() {
@@ -48,14 +43,9 @@ function frufruDoLogin() {
     setTimeout(() => {
         entrada.classList.add("none")
     }, 3000);
-
-    /* namePrompt = {
-        name: nomezin
-    } */
 }
 
 function erroLogin(error) {
-
     console.log(error.response.status);
     setTimeout(() => {
         if (error.response.status === 400) {
@@ -67,31 +57,24 @@ function erroLogin(error) {
             entrada.classList.remove("none")
             loginContainer.classList.remove("none")
             loader.classList.add("none")
-
         }
-
     }, 3000);
-
 }
 
 function manterLog() {
-
     const promise = axios.post(`https://mock-api.driven.com.br/api/v6/uol/status`, namePrompt)
 
 }
-
 // etapa 1 - buscar msgs no servidor (API)
 function buscarMensagens() {
     console.log(`ordem de execução 1 - buscarMensagens`)
     const promessa = axios.get(`https://mock-api.driven.com.br/api/v6/uol/messages`);
 
     promessa
-        //.catch(alertaErro)
         .then(popularMsgServidor);
 }
 
 // etapa 2 - Jogar as mensagens do API na variável msgServidor
-
 function popularMsgServidor(resposta) {
     console.log(`ordem de execução 2 - popularMsgServidor`)
     msgServidor = resposta.data
@@ -100,25 +83,11 @@ function popularMsgServidor(resposta) {
 
 }
 // etapa 3 - itera o array msgServidor
-
 function plotarDoServidor() {
-
-
     console.log("orden de execução 3 - plotarDoServidor")
-
     let ul = document.querySelector("ul.board");
-
-    /* {
-        from: "João",
-        to: "Todos",
-        text: "entra na sala...",
-        type: "status",
-        time: "08:01:17"
-    } */
-
     ul.innerHTML = ``;
     for (let i = 0; i < msgServidor.length; i++) {
-
         // COLOCAR CONDICIONAL DE RESERVADA
         const tipo = msgServidor[i].type
         const destino = msgServidor[i].to
@@ -163,28 +132,28 @@ function plotarDoServidor() {
 
     }
     console.log(msgServidor)
-    // RELOAD DAS MENSAGENS
-    // scroll da msg
     scroll();
 }
-
 // etapa 4 - cadastra msg para enviar AINDA NAO VOU FOCAR AQUI
-
 function enviar() {
     //pegar infos do menu
     const aux = document.querySelector(".participantes .check")
-    const destino = aux.previousElementSibling.innerHTML
-    
     const aux2 = document.querySelector(".messageStatus .check")
+    
+    if(aux === null || aux2 === null){
+        alert("seleciona um contato e um tipo de mensagem")
+        return;
+    }
+
+    const destino = aux.previousElementSibling.innerHTML
     const innerzin = aux2.previousElementSibling.innerHTML
     let tipo;
-    if(innerzin === "Todos"){
+
+    if (innerzin === "Todos") {
         tipo = "message"
-    }else{
+    } else {
         tipo = "private_message"
     }
-    
-     
     //pegar infos do input
     const msgInput = document.querySelector(".msg");
     msg = msgInput.value
@@ -206,12 +175,7 @@ function enviar() {
     msgInput.value = ``;
 }
 
-
-
-
 function erroMessage(error) {
-
-    console.log(error.response.status);
     if (error.response.status === 400) {
         console.log("erro de mensagem")
         window.location.reload();
@@ -222,7 +186,6 @@ function erroMessage(error) {
 function buscarParticipantes() {
     const promise = axios.get(`https://mock-api.driven.com.br/api/v6/uol/participants`)
     promise
-        //.catch(alertaErro)
         .then(popularParticipantes)
 }
 
@@ -232,11 +195,9 @@ function popularParticipantes(resposta) {
 }
 
 function plotarParticipantes() {
-
     console.log(participantes)
     divzin = document.querySelector(".participantes div")
-
-     divzin.innerHTML = ``;
+    divzin.innerHTML = ``;
 
     for (let i = 0; i < participantes.length; i++) {
         divzin.innerHTML += ` 
@@ -246,33 +207,26 @@ function plotarParticipantes() {
                     <img src="/imagens/check.png" class="hidden">
                 </li>
             `;
-    } 
+    }
 }
 
-function atribuirNome(texto){
+function atribuirNome(texto) {
     nomeDestino = texto
     //console.log(nomeDestino)
 }
 
 function selecionarContato(element) {
-    const participante = document.querySelector(".participantes .check")
-    //console.log(participante)
-
+    //const participante = document.querySelector(".participantes .check")
     const contato = document.querySelector(".participantes img.check")
     const texto = element.querySelector(".participantes span").innerHTML
     const check = element.querySelector("img")
-    
-
     if (contato !== null) {
         contato.classList.remove("check");
         check.classList.add("check");
     }
     check.classList.add("check")
-
     atribuirNome(texto);
 }
-
-
 
 function selecionarStatus(element) {
     const messageStatus = document.querySelector(".messageStatus img.check");
@@ -292,7 +246,6 @@ function scroll() {
 }
 
 function hora() {
-
     let hour = new Date().getHours();
     let minutes = new Date().getMinutes();
     let seconds = new Date().getSeconds();
@@ -309,7 +262,7 @@ function chamarMenu() {
     menu.classList.add("show-menu")
 }
 
-function fecharMenu(){
+function fecharMenu() {
     const overlay = document.querySelector(".overlay")
     const menu = document.querySelector(".menu")
 
@@ -323,12 +276,6 @@ function atualizarBoard() {
     setTimeout(manterLog, 5000);
 
 
-}
-function analisarCheck() {
-    /* ESSA FUNÇÃO É PRO BONUS
-    QUANDO TIVER COM CHECK NO MENU
-    VAI AVISAR NO INPUT PRA QUEM ESTÁ ENVIANDO
-    E O TIPO DA MSG */
 }
 
 function enterMessage(evento) {
